acbe8840fdfe9a87dc68a9c4917baf8c1243452b
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-09-13 11:15:12 -0300

    :sparkles: feat: game card component created

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { View, Image } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
+
+import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
@@ -14,7 +17,9 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
-      ></Heading>
+      />
+
+      <GameCard data={GAMES[0]} />
     </View>
   );
 }
